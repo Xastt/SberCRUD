3132091f8f16e25f10df922f5fe8070a59828094
Xast <artemhasrovyan@gmail.com>
2025-02-17 17:09:52 +0300

    Finish

--- src/test/java/xast/sbertasks/SberCRUD/SberCrudApplicationTests.java
+++ src/test/java/xast/sbertasks/SberCRUD/SberCrudApplicationTests.java
@@ -1,13 +1,40 @@
 package xast.sbertasks.SberCRUD;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SberCrudApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void test1() {
+		System.setProperty("webdriver.edge.driver", "D:\\Downloads\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://localhost:7070/person");
+
+		assertEquals("CRUD App", driver.getTitle());
+	}
+
+	@Test
+	public void test2() {
+		System.setProperty("webdriver.edge.driver", "D:\\Downloads\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://localhost:7070/person/new");
+
+		WebElement surnameField = driver.findElement(By.id("surname"));
+		WebElement nameField = driver.findElement(By.id("name"));
+		WebElement ageField = driver.findElement(By.id("age"));
+
+		System.out.println("Surname field: " + surnameField.getAttribute("value"));
+		System.out.println("Name field: " + nameField.getAttribute("value"));
+		System.out.println("Age field: " + ageField.getAttribute("value"));
+
+		assertEquals("Ivanov", surnameField.getAttribute("value"));
+		assertEquals("Ivan", nameField.getAttribute("value"));
 	}
 
 }
